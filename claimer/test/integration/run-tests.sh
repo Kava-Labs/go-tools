206de7dae3cec138fb1bd2db89a077b848692b3d
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-12-17 19:51:08 +0000

    hook up claimer integration tests

---
+++ claimer/test/integration/run-tests.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# Note: this script should be run from within its directory (otherwise it can't find the docker-compose.yaml file)
+
+# exit on errors
+set -e
+
+# start up the deputy and chains
+docker-compose up -d
+
+# wait until the chains are operational
+echo "waiting for kava node to start"
+while ! docker-compose exec kavanode curl --fail localhost:26657/status > /dev/null
+do
+    sleep 1
+done
+# wait until a block is committed
+sleep 4
+echo "done"
+
+# run tests
+# don't exit on error, just capture exit code (https://stackoverflow.com/questions/11231937/bash-ignoring-error-for-a-particular-command)
+# use -count=1 to disable test result caching
+go test . -count=1 -tags integration -v && exitStatus=$? || exitStatus=$?
+# go test . -count=1 -tags integration -v -covermode=atomic -coverpkg="github.com/kava-labs/go-tools/deputy-claimer/claim" -coverprofile=coverage.out && exitStatus=$? || exitStatus=$?
+
+# remove the deputy and chains
+docker-compose down
+
+exit $exitStatus
