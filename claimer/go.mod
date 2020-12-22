416b2a55b9e7a39b929e61e6cecb4a0b495b9876
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-12-22 15:39:46 +0000

    add kava integration test

--- claimer/go.mod
+++ claimer/go.mod
@@ -7,10 +7,14 @@ require (
 	github.com/gorilla/mux v1.7.4
 	github.com/kava-labs/binance-chain-go-sdk v1.2.5-kava
 	github.com/kava-labs/go-sdk v0.3.0-rc1
+	github.com/kava-labs/go-tools/deputy-claimer v0.0.0-00010101000000-000000000000
 	github.com/kava-labs/kava v0.11.0-rc1
 	github.com/kava-labs/tendermint v0.32.3-kava1
 	github.com/sirupsen/logrus v1.4.2
+	github.com/stretchr/testify v1.6.1
 	github.com/tendermint/go-amino v0.15.1
 	github.com/tendermint/tendermint v0.33.7
 	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 )
+
+replace github.com/kava-labs/go-tools/deputy-claimer => ../deputy-claimer
--- claimer/test/integration/concurrency_test.go
+++ claimer/test/integration/concurrency_test.go
@@ -2,8 +2,127 @@
 
 package integration
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+	"time"
 
-func TestPlaceholder(t *testing.T) {
-	t.Fail()
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	bnbtypes "github.com/kava-labs/binance-chain-go-sdk/common/types"
+	"github.com/kava-labs/go-sdk/client"
+	"github.com/kava-labs/go-tools/deputy-claimer/test/addresses"
+	"github.com/kava-labs/go-tools/deputy-claimer/test/swap"
+	"github.com/kava-labs/kava/app"
+	kavatypes "github.com/kava-labs/kava/x/bep3/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/kava-labs/go-tools/claimer/config"
+	"github.com/kava-labs/go-tools/claimer/renamethis"
+)
+
+func TestMain(m *testing.M) {
+	sdkConfig := sdk.GetConfig()
+	app.SetBech32AddressPrefixes(sdkConfig)
+
+	os.Exit(m.Run())
+}
+func TestClaimConcurrentSwapsKava(t *testing.T) {
+
+	addrs := addresses.GetAddresses()
+
+	kavaSwapper := swap.NewKavaSwapClient(addresses.KavaNodeURL)
+	swapBuilder := swap.NewDefaultSwapBuilder(
+		addrs.Kava.Deputys.Bnb.HotWallet.Mnemonic,
+		addrs.Bnb.Deputys.Bnb.HotWallet.Mnemonic,
+	)
+
+	createKavaSwap := func() (swap.CrossChainSwap, error) {
+		swap := swapBuilder.NewBnbToKavaSwap(
+			addrs.Bnb.Users[0].Mnemonic,
+			addrs.Kava.Users[0].Address,
+			bnbtypes.Coins{{Denom: "BNB", Amount: 500_000_000}},
+		)
+		// only need the receiving chain swap created
+		_, err := kavaSwapper.Create(swap.KavaSwap, client.Commit)
+		if err != nil {
+			return swap, err
+		}
+		return swap, nil
+	}
+
+	numConcurrentSwaps := 8
+	swaps := []swap.CrossChainSwap{}
+	for i := 0; i < numConcurrentSwaps; i++ {
+		s, err := createKavaSwap()
+		t.Logf("created kava swap %x", s.KavaSwap.GetSwapID())
+		require.NoError(t, err)
+		swaps = append(swaps, s)
+	}
+
+	cfg := config.Config{
+		Kava: config.KavaConfig{
+			ChainID:   "kava-localnet",
+			Endpoint:  addresses.KavaNodeURL,
+			Mnemonics: kavaUserMenmonics(addrs)[2:],
+		},
+		BinanceChain: config.BinanceChainConfig{
+			ChainID:  "Binance-Chain-Tigris",
+			Endpoint: addresses.BnbNodeURL,
+			Mnemonic: bnbUserMenmonics(addrs)[0],
+		},
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go renamethis.Main(ctx, cfg)
+	time.Sleep(1 * time.Second) // give time for the server to start
+
+	for _, s := range swaps {
+		err := sendClaimRequest("kava", s.KavaSwap.GetSwapID(), s.RandomNumber)
+		require.NoError(t, err)
+	}
+	time.Sleep(20 * time.Second) // give time for last swap to be claimed
+
+	for _, s := range swaps {
+		status, err := kavaSwapper.FetchStatus(s.KavaSwap)
+		require.NoError(t, err)
+		require.Equalf(t, kavatypes.Completed, status, "expected swap status '%s', actual '%s'", kavatypes.Completed, status)
+	}
+}
+
+func sendClaimRequest(chain string, swapID []byte, randomNumber []byte) error {
+	resp, err := http.Post(
+		fmt.Sprintf(
+			"http://localhost:8080/claim?target-chain=%s&swap-id=%x&random-number=%x",
+			chain,
+			swapID,
+			randomNumber,
+		),
+		"",
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("claim request failed: %s", resp.Status)
+	}
+	return nil
+}
+
+func kavaUserMenmonics(addrs addresses.Addresses) []string {
+	mnemonics := []string{}
+	for _, u := range addrs.Kava.Users {
+		mnemonics = append(mnemonics, u.Mnemonic)
+	}
+	return mnemonics
+}
+func bnbUserMenmonics(addrs addresses.Addresses) []string {
+	mnemonics := []string{}
+	for _, u := range addrs.Bnb.Users {
+		mnemonics = append(mnemonics, u.Mnemonic)
+	}
+	return mnemonics
 }
