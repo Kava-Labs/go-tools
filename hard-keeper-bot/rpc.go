a9adf72259d3cfa3e46b027808fee9b6e6deba56
Nick DeLuca <nickdeluca08@gmail.com>
2021-02-23 09:55:38 -0600

    move rpc interface and parsing function out of liquidation client

---
+++ hard-keeper-bot/rpc.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/tendermint/tendermint/libs/bytes"
+	rpcclient "github.com/tendermint/tendermint/rpc/client"
+	ctypes "github.com/tendermint/tendermint/rpc/core/types"
+)
+
+type RpcClient interface {
+	Status() (*ctypes.ResultStatus, error)
+	ABCIQueryWithOptions(
+		path string,
+		data bytes.HexBytes,
+		opts rpcclient.ABCIQueryOptions,
+	) (*ctypes.ResultABCIQuery, error)
+}
+
+func ParseABCIResult(result *ctypes.ResultABCIQuery, err error) ([]byte, error) {
+	if err != nil {
+		return []byte{}, err
+	}
+
+	resp := result.Response
+	if !resp.IsOK() {
+		return []byte{}, errors.New(resp.Log)
+	}
+
+	value := result.Response.GetValue()
+	// TODO: why do we check length here?
+	if len(value) == 0 {
+		return []byte{}, nil
+	}
+
+	return value, nil
+}
