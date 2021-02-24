c94be574fbdf8399fb83c5261e6b750641ca4e07
Nick DeLuca <nickdeluca08@gmail.com>
2021-02-24 15:30:22 -0600

    update rpc interface

--- hard-keeper-bot/rpc.go
+++ hard-keeper-bot/rpc.go
@@ -6,15 +6,21 @@ import (
 	"github.com/tendermint/tendermint/libs/bytes"
 	rpcclient "github.com/tendermint/tendermint/rpc/client"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
+	tmtypes "github.com/tendermint/tendermint/types"
 )
 
 type RpcClient interface {
 	Status() (*ctypes.ResultStatus, error)
+	ABCIQuery(
+		path string,
+		data bytes.HexBytes,
+	) (*ctypes.ResultABCIQuery, error)
 	ABCIQueryWithOptions(
 		path string,
 		data bytes.HexBytes,
 		opts rpcclient.ABCIQueryOptions,
 	) (*ctypes.ResultABCIQuery, error)
+	BroadcastTxSync(tx tmtypes.Tx) (*ctypes.ResultBroadcastTx, error)
 }
 
 func ParseABCIResult(result *ctypes.ResultABCIQuery, err error) ([]byte, error) {
