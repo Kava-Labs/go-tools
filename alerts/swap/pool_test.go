6d5d8b9f4aaf1f4a8f58f01ff423a47dd525f5df
drklee3 <derrick@dlee.dev>
2021-08-30 16:42:19 -0700

    Use test tables with subtests

--- alerts/swap/pool_test.go
+++ alerts/swap/pool_test.go
@@ -1,21 +1,20 @@
 package swap
 
 import (
+	"fmt"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/assert"
 )
 
-type percentChangeValue struct {
-	a   int64
-	b   int64
-	exp int64
-}
-
 func TestPercentChange(t *testing.T) {
 	// Expected values checked against an online calculator
-	values := []percentChangeValue{
+	values := []struct {
+		giveAValue        int64
+		giveBValue        int64
+		wantPercentChange int64
+	}{
 		{5, 5, 0},
 		{5, 1, -80},      // 5 -> 1
 		{1, 5, 400},      // 1 -> 5
@@ -24,44 +23,52 @@ func TestPercentChange(t *testing.T) {
 		{0, 1, 1},        // Should fail if first is zero
 	}
 
-	for _, val := range values {
-		diff, err := PercentChange(sdk.NewDec(val.a), sdk.NewDec(val.b))
-		if err != nil {
-			// Should only error if the first value is 0
-			assert.Equal(t, int64(0), val.a)
-			continue
-		}
+	for _, tt := range values {
+		t.Run(fmt.Sprintf("%v->%v", tt.giveAValue, tt.giveBValue), func(t *testing.T) {
+			diff, err := PercentChange(sdk.NewDec(tt.giveAValue), sdk.NewDec(tt.giveBValue))
+			if err != nil {
+				// Should only error if the first value is 0
+				assert.Equal(t, int64(0), tt.giveAValue)
+				return
+			}
 
-		// Multiply by 100 such that 100 == 100%
-		assert.Equal(t, val.exp, diff.MulInt64(100).RoundInt64())
+			// Multiply by 100 such that 100 == 100%
+			assert.Equal(t, tt.wantPercentChange, diff.MulInt64(100).RoundInt64())
+		})
 	}
 }
 
-type poolAssetValue struct {
-	a    sdk.Coin
-	b    sdk.Coin
-	bUsd sdk.Dec
-	exp  int64
-}
-
 func TestGetPoolAssetPrice(t *testing.T) {
-	values := []poolAssetValue{
+	values := []struct {
+		giveACoin  sdk.Coin
+		giveBCoin  sdk.Coin
+		giveBUsd   sdk.Dec
+		wantAPrice int64
+	}{
 		{
-			sdk.Coin{Denom: "usd", Amount: sdk.NewInt(10)}, sdk.Coin{Denom: "usd", Amount: sdk.NewInt(10)},
-			sdk.NewDec(1), 1,
+			sdk.Coin{Denom: "usd", Amount: sdk.NewInt(10)},
+			sdk.Coin{Denom: "usd", Amount: sdk.NewInt(10)},
+			sdk.NewDec(1),
+			1,
 		},
 		{
-			sdk.Coin{Denom: "bnb", Amount: sdk.NewInt(100000000)}, sdk.Coin{Denom: "usdx", Amount: sdk.NewInt(492000000)},
-			sdk.NewDec(1), 492,
+			sdk.Coin{Denom: "bnb", Amount: sdk.NewInt(100000000)},
+			sdk.Coin{Denom: "usdx", Amount: sdk.NewInt(492000000)},
+			sdk.NewDec(1),
+			492,
 		},
 		// Some actual pool values
 		{
-			sdk.Coin{Denom: "bnb", Amount: sdk.NewInt(19247262658)}, sdk.Coin{Denom: "usdx", Amount: sdk.NewInt(91486075424)},
-			sdk.NewDec(1), 475,
+			sdk.Coin{Denom: "bnb", Amount: sdk.NewInt(19247262658)},
+			sdk.Coin{Denom: "usdx", Amount: sdk.NewInt(91486075424)},
+			sdk.NewDec(1),
+			475,
 		},
 		{
-			sdk.Coin{Denom: "btcb", Amount: sdk.NewInt(495726043)}, sdk.Coin{Denom: "usdx", Amount: sdk.NewInt(239260237273)},
-			sdk.NewDec(1), 48265,
+			sdk.Coin{Denom: "btcb", Amount: sdk.NewInt(495726043)},
+			sdk.Coin{Denom: "usdx", Amount: sdk.NewInt(239260237273)},
+			sdk.NewDec(1),
+			48265,
 		},
 	}
 
@@ -73,14 +80,16 @@ func TestGetPoolAssetPrice(t *testing.T) {
 		},
 	}
 
-	for _, val := range values {
-		usdValue, err := GetPoolAssetUsdPrice(poolData, val.a, val.b, val.bUsd)
-		if err != nil {
-			// Should only error if the first value is 0
-			assert.Equal(t, int64(0), val.a)
-			continue
-		}
+	for _, tt := range values {
+		t.Run(fmt.Sprintf("%v:%v", tt.giveACoin, tt.giveBCoin), func(t *testing.T) {
+			usdValue, err := GetPoolAssetUsdPrice(poolData, tt.giveACoin, tt.giveBCoin, tt.giveBUsd)
+			if err != nil {
+				// Should only error if the first value is 0
+				assert.Equal(t, int64(0), tt.giveACoin)
+				return
+			}
 
-		assert.Equal(t, val.exp, usdValue.RoundInt64())
+			assert.Equal(t, tt.wantAPrice, usdValue.RoundInt64())
+		})
 	}
 }
