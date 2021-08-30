adccfffe519b8b01a4e5738ba507993e9a3320f1
drklee3 <derrick@dlee.dev>
2021-08-30 09:01:52 -0700

    Remove unused rpc func

--- alerts/rpc/client.go
+++ alerts/rpc/client.go
@@ -1,13 +1,12 @@
 package rpc
 
 import (
-	"errors"
-
 	"github.com/tendermint/tendermint/libs/bytes"
 	rpcclient "github.com/tendermint/tendermint/rpc/client"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 )
 
+// RpcClient defines an interface that can query information via ABCI
 type RpcClient interface {
 	Status() (*ctypes.ResultStatus, error)
 	ABCIQueryWithOptions(
@@ -15,22 +14,4 @@ type RpcClient interface {
 		data bytes.HexBytes,
 		opts rpcclient.ABCIQueryOptions,
 	) (*ctypes.ResultABCIQuery, error)
-}
-
-func ParseABCIResult(result *ctypes.ResultABCIQuery, err error) ([]byte, error) {
-	if err != nil {
-		return []byte{}, err
-	}
-
-	resp := result.Response
-	if !resp.IsOK() {
-		return []byte{}, errors.New(resp.Log)
-	}
-
-	value := result.Response.GetValue()
-	if value == nil {
-		return []byte{}, nil
-	}
-
-	return value, nil
 }
