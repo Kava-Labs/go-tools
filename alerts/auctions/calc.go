b77bcbcabf1036944b408533657741d57c996dd4
karzak <kjydavis3@gmail.com>
2022-03-30 14:15:28 -0600

    feat: add inefficient auction alert to alert service

--- alerts/auctions/calc.go
+++ alerts/auctions/calc.go
@@ -2,8 +2,10 @@ package auctions
 
 import (
 	"fmt"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	auctiontypes "github.com/kava-labs/kava/x/auction/types"
 )
 
 // AuctionInfo contains information about an auction
@@ -36,4 +38,42 @@ func CalculateTotalAuctionsUSDValue(data *AuctionData) (sdk.Dec, error) {
 // calculateUSDValue calculates the USD value of a given Coin and AssetInfo
 func CalculateUSDValue(coin sdk.Coin, assetInfo AssetInfo) sdk.Dec {
 	return coin.Amount.ToDec().Quo(assetInfo.ConversionFactor.ToDec()).Mul(assetInfo.Price)
+}
+
+func CheckInefficientAuctions(data *AuctionData, thresholdUSD, thresholdRatio sdk.Dec, thresholdTime time.Duration) ([]auctiontypes.Auction, error) {
+	inefficientAuctions := []auctiontypes.Auction{}
+	for _, auction := range data.Auctions {
+		// check if the time remaining on the auction is below the threshold
+		endTime := auction.GetEndTime()
+		timeRemaining := time.Until(endTime)
+		if timeRemaining > thresholdTime {
+			continue
+		}
+
+		// check that the lot USD value is above the threshold
+		lot := auction.GetLot()
+		lotAssetInfo, ok := data.Assets[lot.Denom]
+		if !ok {
+			return []auctiontypes.Auction{}, fmt.Errorf("missing asset info for %s", lot.Denom)
+		}
+		usdValueLot := CalculateUSDValue(lot, lotAssetInfo)
+		if usdValueLot.LT(thresholdUSD) {
+			continue
+		}
+
+		// check that the bidUSD:lotUSD ratio is below the threshold
+		bid := auction.GetBid()
+		bidAssetInfo, ok := data.Assets[bid.Denom]
+		if !ok {
+			return []auctiontypes.Auction{}, fmt.Errorf("missing asset info for %s", bid.Denom)
+		}
+		usdValueBid := CalculateUSDValue(bid, bidAssetInfo)
+		ratio := usdValueBid.Quo(usdValueLot)
+		if ratio.GTE(thresholdRatio) {
+			continue
+		}
+		inefficientAuctions = append(inefficientAuctions, auction)
+
+	}
+	return inefficientAuctions, nil
 }
--- alerts/config/config.go
+++ alerts/config/config.go
@@ -1,15 +1,18 @@
 package config
 
 const (
-	kavaRpcUrlEnvKey        = "KAVA_RPC_URL"
-	dynamoDbTableNameEnvKey = "DYNAMODB_TABLE_NAME"
-	slackTokenEnvKey        = "SLACK_TOKEN"
-	slackChannelIdEnvKey    = "SLACK_CHANNEL_ID"
-	intervalEnvKey          = "INTERVAL"
-	alertFrequencyEnvKey    = "ALERT_FREQUENCY"
-	usdThresholdEnvKey      = "USD_THRESHOLD"
-	usdxDeviationEnvKey     = "USDX_DEVIATION"
-	usdxBasePriceKey        = "USDX_BASE_PRICE"
+	kavaRpcUrlEnvKey               = "KAVA_RPC_URL"
+	dynamoDbTableNameEnvKey        = "DYNAMODB_TABLE_NAME"
+	slackTokenEnvKey               = "SLACK_TOKEN"
+	slackChannelIdEnvKey           = "SLACK_CHANNEL_ID"
+	intervalEnvKey                 = "INTERVAL"
+	alertFrequencyEnvKey           = "ALERT_FREQUENCY"
+	usdThresholdEnvKey             = "USD_THRESHOLD"
+	inefficientThresholdEnvKey     = "INEFFICIENT_AUCTION_USD_THRESHOLD"  // the USD of the auction lot for the alerter to care about it -> If an auction's lot is above the threshold, the alert can be triggered
+	inefficientRatioEnvKey         = "INEFFICIENT_AUCTION_RATIO"          // the ratio of bid:lot in USD. Below this ratio, the alert can be triggered
+	inefficientTimeRemainingEnvKey = "INEFFICIENT_AUCTION_TIME_THRESHOLD" // the amount of time remaining in the auction. If below this duration, the alert can be triggered
+	usdxDeviationEnvKey            = "USDX_DEVIATION"
+	usdxBasePriceKey               = "USDX_BASE_PRICE"
 )
 
 // ConfigLoader provides an interface for loading config values from a provided
